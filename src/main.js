4198eeb9d64a59a3e330148a2e09c7e2d4a39f58
Marcelo Pereira da Silva <marcelopspereira@gmail.com>
2018-04-28 18:40:58 -0300

    configurações iniciais

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+
+
+new Vue({
+  el: "#app",
+  data: {
+    titulo: "Minha primeira aplicação Vue.js 2",
+    bool: false,
+    numeroInteiro: 10,
+    numeroFloat: 20.1,
+    object: {
+      name: "Marcelo PS"
+    },
+    link: "http://localhost:8080"
+  }
+});
